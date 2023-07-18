d499b6fb3c78f0ff2b54aeffe92d8ae2def12dd9
Bhavesh Yadav <codezera3@gmail.com>
2023-07-18 17:33:07 +0530

    fixed a bug in pagination bar

--- src/components/PaginationBar.tsx
+++ src/components/PaginationBar.tsx
@@ -2,39 +2,51 @@ import Link from "next/link";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
 interface PaginationBarProps {
-    currentPage: number,
-    totalPages: number
+  currentPage: number;
+  totalPages: number;
 }
 
-const PaginationBar = ({ currentPage, totalPages }: PaginationBarProps) => {
+export default function PaginationBar({
+  currentPage,
+  totalPages,
+}: PaginationBarProps) {
+  const maxPage = Math.min(totalPages, Math.max(currentPage + 4, 10));
+  const minPage = Math.max(1, Math.min(currentPage - 5, maxPage - 9));
 
-    const maxPage = Math.min(totalPages, Math.max(currentPage + 4, 10));
-    const minPage = Math.max(1, Math.min(currentPage - 5, maxPage - 9));
+  const numberedPageItems: JSX.Element[] = [];
 
-    const numberedPageItems: JSX.Element[] = [];
+  for (let page = minPage; page <= maxPage; page++) {
+    numberedPageItems.push(
+      <Link
+        href={"?page=" + page}
+        key={page}
+        className={`join-item btn ${
+          currentPage === page ? "btn-active pointer-events-none" : ""
+        }`}
+      >
+        {page}
+      </Link>
+    );
+  }
 
-    for (let page = minPage; page < maxPage; page++){
-        numberedPageItems.push(
-            <Link href={"?page="+page} key={page} className={`join-item btn ${currentPage === page ? "btn-active pointer-events-none": ""}`}>
-            {page}
-            </Link>
-        )
-    }
-
-    return (
-        <>
-            <div className="join hidden sm:block">{numberedPageItems}</div>
-            <div className="join block sm:hidden">
-                { currentPage > 1 && (
-                    <Link href={"?page=" + (currentPage - 1)}> <AiOutlineArrowLeft/> </Link>
-                )}
-                <div className="join-item btn pointer-events-none">{currentPage}</div>
-                { currentPage < totalPages && (
-                    <Link href={"?page=" + (currentPage + 1)}> <AiOutlineArrowRight/> </Link>
-                )}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="join hidden sm:block">{numberedPageItems}</div>
+      <div className="join block sm:hidden">
+        {currentPage > 1 && (
+          <Link href={"?page=" + (currentPage - 1)} className="join-item btn">
+            <AiOutlineArrowLeft />
+          </Link>
+        )}
+        <button className="join-item btn pointer-events-none">
+          Page {currentPage}
+        </button>
+        {currentPage < totalPages && (
+          <Link href={"?page=" + (currentPage + 1)} className="join-item btn">
+            <AiOutlineArrowRight />
+          </Link>
+        )}
+      </div>
+    </>
+  );
 }
-
-export default PaginationBar
